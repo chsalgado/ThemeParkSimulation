e573e50d26a77c9d4bd507face3ecc143060e2a0
Borui Zhang <bzha@amazon.com>
2020-03-11 20:57:49 -0700

    d

--- ConsoleApp1/Models/Visitor.cs
+++ ConsoleApp1/Models/Visitor.cs
@@ -9,6 +9,24 @@ namespace ConsoleApp1.Models
 {
     public class Visitor
     {
+        private static Tuple<double, double>[] PAYOFF_RANGES = new Tuple<double, double>[]
+        {
+
+        };
+
+        private static readonly List<AttractionCategory> ATTRACTION_CATEGORIES
+            = Enum.GetValues(typeof(AttractionCategory)).Cast<AttractionCategory>().ToList();
+
+        private static readonly List<Func<IVisitorStrategy>> STRATEGY_CREATORS = new List<Func<IVisitorStrategy>>
+        {
+            () => new DistanceVisitorStrategy(),
+            () => new PayoffVisitorStrategy(),
+            () => new WaitTimeVisitorStrategy()
+
+        };
+
+        private Random randomizer = new Random();
+
         public double AccruedPayoff { get; set; }
 
         public IDictionary<Attraction, double> AttractionPayoffMap { get; set; }
@@ -26,12 +44,34 @@ namespace ConsoleApp1.Models
 
         public Point Location { get; set; }
 
-        public void Init()
+        public void Init(List<Attraction> attractions)
         {
             // Fill category preferences
+            var attractionCategories = ATTRACTION_CATEGORIES.OrderBy(a => randomizer.NextDouble()).ToList();
+            var payoffRangeForCategory = new Dictionary<AttractionCategory, Tuple<double, double>>();
+            for (int i = 0; i < attractionCategories.Count(); i ++)
+            {
+                payoffRangeForCategory.Add(attractionCategories[i], PAYOFF_RANGES[i]);
+            }
+
             // Fill payoff maps
+            AttractionPayoffMap = new Dictionary<Attraction, double> ();
+            foreach (var attraction in attractions)
+            {
+                var ranges = payoffRangeForCategory[attraction.AttractionCategory];
+                var min = ranges.Item1;
+                var max = ranges.Item2;
+                var payoff = randomizer.NextDouble() * (max - min) + min;
+                AttractionPayoffMap[attraction] = payoff;
+            }
+
             // Get an IVisitorStrategy
+            VisitorStrategy = STRATEGY_CREATORS[randomizer.Next(STRATEGY_CREATORS.Count())].Invoke();
+
             // Set Location to 0, 0
+            Location = new Point(0, 0);
+
+            AccruedPayoff = 0;
         }
 
         public bool AcceptIncentive(Incentive incentive)
