dbd3da8efa9f984ac056d7d9a5e88a1a8f9b5448
Christian Salgado <chsalgad@microsoft.com>
2020-03-13 17:10:45 -0600

    IIncentiveGenerationStrategy

---
+++ ConsoleApp1/IncentiveGenerationStrategies/IIncentiveGenerationStrategy.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Strategies
+{
+    public interface IIncentiveGenerationStrategy
+    {
+        Incentive GenerateIncentive(IEnumerable<Attraction> attractions, double totalBugdet, double usedBudget, int operationHours, int currentTime);
+    }
+}
---
+++ ConsoleApp1/IncentiveGenerationStrategies/WaitTimeBalanceGenerationStrategy.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1.Strategies
+{
+    public class WaitTimeBalanceGenerationStrategy : IIncentiveGenerationStrategy
+    {
+        public Incentive GenerateIncentive(IEnumerable<Attraction> attractions, double totalBugdet, double usedBudget, int operationHours, int currentTime)
+        {
+            return null;
+        }
+    }
+}
